3e130002ffa53fd2ba07c988068e0882193849d0
czarstark <trevor.balint@gmail.com>
2017-04-21 21:08:00 +0100

    Basic api functionality Given a fixed list of lat/longs, get a full nXn dist mat for them

--- mapsApi/distanceMatrixGetter.py
+++ mapsApi/distanceMatrixGetter.py
@@ -4,12 +4,24 @@ Method returns pandas array of dist mat
 """
 
 import googlemaps
+from pandas.io.json import json_normalize
+
 
 # Read API key from file
-f = open("C:\Users\TrevHP\Dropbox\MDSproject\mapsAPIkey.txt",'r')
-mapsKey = f.readline()
-f.close()
+def getkey():
+    f = open("C:\Users\TrevHP\Dropbox\MDSproject\mapsDistMatAPIkey.txt",'r')
+    mapsKey = f.readline()
+    f.close()
+    return mapsKey
 
 
-gmaps = googlemaps.Client(key=mapsKey)
+gmaps = googlemaps.Client(key=getkey())
 
+destinations = "54.008102,-1.536223|54.002248,-1.522599|53.9983792,-1.509521"
+
+matrix = gmaps.distance_matrix(destinations, destinations, units="metric")
+newmat = json_normalize(matrix,['rows', 'elements'])
+if newmat['status'].unique().size > 1: quit("ARGHS")
+else: print "Everything is awesome!"
+print newmat
+#geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
