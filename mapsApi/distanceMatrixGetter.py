b59e22f38eb8d7025d2339780b1653a505820500
czarstark <trevor.balint@gmail.com>
2017-05-05 16:22:41 +0100

    Getting distance matrix from API Removed the initial DF assignment - unnecessary to fill with 0/1/-1 Saving distance mat to csv so we don't have to query API again

--- mapsApi/distanceMatrixGetter.py
+++ mapsApi/distanceMatrixGetter.py
@@ -35,44 +35,48 @@ studentData = readstudentdata()
 fromToMatrix = pd.DataFrame(columns=studentData["oa11"], index=studentData["oa11"])
 fromToMatrix = fromToMatrix.fillna(-1)
 
-logging.debug("Done. Creating initial distance dataframe...")
-
-# Create the initial distance dataframe, filling the top half with 1's and the diagonal with 0's
-# todo is this step even necessary?
-for idx1 in range(0, fromToMatrix.index.size):
-    fromToMatrix.iloc[idx1, idx1] = 0
-    for idx2 in range(idx1+1, fromToMatrix.index.size):
-        fromToMatrix.iloc[idx1, idx2] = 1
+logging.debug("Done. Creating API request strings...")
 
 apiMatrix = []
-# Need a separate loop to create the origin/destination strings for the API
+# Need a loop to create the origin/destination strings for the API
 for idx1 in range(0, fromToMatrix.index.size):
     destString = ""
     for idx2 in range(0, fromToMatrix.index.size):
         if idx1 != idx2:
             destString += str(studentData.iloc[idx2]["lat"]) + "," + str(studentData.iloc[idx2]["long"]) + "|"
     originString = str(studentData.iloc[idx1]["lat"]) + "," + str(studentData.iloc[idx1]["long"])
-    apiMatrix += [[originString, destString]]
+    apiMatrix += [[originString, destString[:-1]]] # The :-1 is to slice off the | from the last added string
 
-logging.debug("Done.")
+logging.debug("Done. Starting API Requests...")
 
 
 # Start google maps session
 gmaps = googlemaps.Client(key=getkey())
 
-matrix = gmaps.distance_matrix(origins, destinations, units="metric")
-newmat = json_normalize(matrix, ['rows', 'elements'])
+for idx1 in range(0, fromToMatrix.index.size):
+    matrix = gmaps.distance_matrix(apiMatrix[idx1][0], apiMatrix[idx1][1], units="metric")
+    newmat = json_normalize(matrix, ['rows', 'elements'])
 
-# Verify that the matrix came back with no errors
-if newmat['status'].unique().size > 1 or newmat['status'][0] != "OK":
-    logging.error("Query to mapping API failed")
-    quit()
-# Verify that it's the right size
-elif newmat['distance'].size != len(destinations.split("|")) * len(origins.split("|")):
-    logging.error("Query to mapping API returned matrix of wrong size")
-    quit()
-else:
-    logging.debug("Mapping API query success")
-print newmat
+    # Verify that the query returned with no errors
+    if newmat['status'].unique().size > 1 or newmat['status'][0] != "OK":
+        logging.error("Query to mapping API failed")
+        quit()
+    # Verify that it's the right size
+    elif newmat['distance'].size != len(apiMatrix[idx1][1].split("|")) * len(apiMatrix[idx1][0].split("|")):
+        logging.error("Query to mapping API returned matrix of wrong size")
+        quit()
+    else:
+        logging.debug("API query success")
+
+    # Successful query, so insert the data into the distance matrix
+    pastDiag = 0
+    for idx2 in range(0, fromToMatrix.index.size):
+        if idx1 == idx2:
+            pastDiag = 1
+            fromToMatrix.iloc[idx1, idx1] = 0
+        else:
+            fromToMatrix.iloc[idx1, idx2] = newmat["distance"].iloc[idx2-pastDiag]["value"]
+
+fromToMatrix.to_csv("distanceMatrix.csv")
 
 gmaps.session.close()
