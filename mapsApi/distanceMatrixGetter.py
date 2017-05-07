c0be9a1bb951f05d9cbfe543b22d4bba9ba64bb3
Trevor <trevor.balint@gmail.com>
2017-05-07 09:00:53 +0000

    Combined API string generation with API calls Limited API call to 23 destinations at a time

--- mapsApi/distanceMatrixGetter.py
+++ mapsApi/distanceMatrixGetter.py
@@ -13,12 +13,14 @@ import os
 # Set up logger
 logging.basicConfig(level=gc.loggingLevel, format='%(asctime)s - %(levelname)s - %(message)s')
 
+
 # Read API key from file
 def getkey():
     f = open(gc.dropboxPath + "MDSproject/mapsDistMatAPIkey.txt", 'r')
     mapsKey = f.readline()
     f.close()
     return mapsKey
+
 
 def readstudentdata():
     cur_path = os.path.dirname(__file__)
@@ -35,47 +37,42 @@ studentData = readstudentdata()
 fromToMatrix = pd.DataFrame(columns=studentData["oa11"], index=studentData["oa11"])
 fromToMatrix = fromToMatrix.fillna(-1)
 
-logging.debug("Done. Creating API request strings...")
-
-apiMatrix = []
-# Need a loop to create the origin/destination strings for the API
-for idx1 in range(0, fromToMatrix.index.size):
-    destString = ""
-    for idx2 in range(0, fromToMatrix.index.size):
-        if idx1 != idx2:
-            destString += str(studentData.iloc[idx2]["lat"]) + "," + str(studentData.iloc[idx2]["long"]) + "|"
-    originString = str(studentData.iloc[idx1]["lat"]) + "," + str(studentData.iloc[idx1]["long"])
-    apiMatrix += [[originString, destString[:-1]]] # The :-1 is to slice off the | from the last added string
-
 logging.debug("Done. Starting API Requests...")
-
 
 # Start google maps session
 gmaps = googlemaps.Client(key=getkey())
 
+apiMatrix = []
+# Need a loop to create the origin/destination strings for the API
 for idx1 in range(0, fromToMatrix.index.size):
-    matrix = gmaps.distance_matrix(apiMatrix[idx1][0], apiMatrix[idx1][1], units="metric")
-    newmat = json_normalize(matrix, ['rows', 'elements'])
-
-    # Verify that the query returned with no errors
-    if newmat['status'].unique().size > 1 or newmat['status'][0] != "OK":
-        logging.error("Query to mapping API failed")
-        quit()
-    # Verify that it's the right size
-    elif newmat['distance'].size != len(apiMatrix[idx1][1].split("|")) * len(apiMatrix[idx1][0].split("|")):
-        logging.error("Query to mapping API returned matrix of wrong size")
-        quit()
-    else:
-        logging.debug("API query success")
-
-    # Successful query, so insert the data into the distance matrix
-    pastDiag = 0
+    originString = str(studentData.iloc[idx1]["lat"]) + "," + str(studentData.iloc[idx1]["long"])
+    destString = ""
+    startVal = 0
     for idx2 in range(0, fromToMatrix.index.size):
-        if idx1 == idx2:
-            pastDiag = 1
-            fromToMatrix.iloc[idx1, idx1] = 0
-        else:
-            fromToMatrix.iloc[idx1, idx2] = newmat["distance"].iloc[idx2-pastDiag]["value"]
+        destString += str(studentData.iloc[idx2]["lat"]) + "," + str(studentData.iloc[idx2]["long"]) + "|"
+        # API can only handle requests up to length 25; since we have 230 elements, break it into 23s
+        if idx2 % 23 == 0 and idx2 > 0:
+            # Make the API request
+            matrix = gmaps.distance_matrix(originString, destString[:-1], units="metric")  # [:-1] is to remove last "|"
+            newmat = json_normalize(matrix, ["rows", "elements"])
+
+            # Verify that the query returned with no errors
+            if newmat['status'].unique().size > 1 or newmat['status'][0] != "OK":
+                logging.error("Query to mapping API failed")
+                quit()
+            # Verify that it's the right size
+            elif newmat['distance'].size != len(destString[:-1].split("|")) * len(originString.split("|")):
+                logging.error("Query to mapping API returned matrix of wrong size")
+                quit()
+            else:
+                logging.debug("API query success")
+
+            # Successful query, so insert the data into the distance matrix
+            for idx in range(0, newmat.index.size):
+                fromToMatrix.iloc[idx1, startVal + idx] = newmat["distance"].iloc[idx]["value"]
+
+            startVal = idx2
+            destString = ""
 
 fromToMatrix.to_csv("distanceMatrix.csv")
 
